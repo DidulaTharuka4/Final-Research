8e8eaa54592a431c18666fc8457445c1dfcab9a5
it21371790 <99164099+it21371790@users.noreply.github.com>
2024-12-03 12:05:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+
+import pandas as pd
+import numpy as np
+import altair as alt
+
+import joblib
+
+pipe_lr = joblib.load(open("model/text_emotion.pkl", "rb"))
+
+emotions_emoji_dict = {"anger": "😠", "disgust": "🤮", "fear": "😨😱", "happy": "🤗", "joy": "😂", "neutral": "😐", "sad": "😔",
+                       "sadness": "😔", "shame": "😳", "surprise": "😮"}
+
+
+def predict_emotions(docx):
+    results = pipe_lr.predict([docx])
+    return results[0]
+
+
+def get_prediction_proba(docx):
+    results = pipe_lr.predict_proba([docx])
+    return results
+
+
+def main():
+    st.title("Discover the mental health status ")
+    st.subheader("Identify emotion status from text")
+      
+    with st.form(key='my_form'):
+        raw_text = st.text_area("Type Here")
+        submit_text = st.form_submit_button(label='Submit')
+        
+       
+
+    if submit_text:
+        col1, col2 = st.columns(2)
+
+        prediction = predict_emotions(raw_text)
+        probability = get_prediction_proba(raw_text)
+
+        with col1:
+            st.success("Original Text")
+            st.write(raw_text)
+
+            st.success("Prediction")
+            emoji_icon = emotions_emoji_dict[prediction]
+            st.write("{}:{}".format(prediction, emoji_icon))
+            st.write("Confidence:{}".format(np.max(probability)))
+
+       
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
